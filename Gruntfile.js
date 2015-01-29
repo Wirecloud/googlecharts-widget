61e7f3f2c3f1206c5ac18a9b25bbd554963691c4
Álvaro Arranz <aarranz@fi.upm.es>
2015-01-29 16:59:24 +0100

    Configure the project using grunt

---
+++ Gruntfile.js
@@ -0,0 +1,105 @@
+/*!
+ *   Copyright 2014-2015 CoNWeT Lab., Universidad Politecnica de Madrid
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+
+module.exports = function (grunt) {
+
+    'use strict';
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        compress: {
+            widget: {
+                options: {
+                    mode: 'zip',
+                    archive: 'build/<%= pkg.vendor %>_<%= pkg.name %>_<%= pkg.version %>-dev.wgt'
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'src',
+                        src: [
+                            'css/**/*',
+                            'doc/**/*',
+                            'images/**/*',
+                            'js/**/*',
+                            'lib/**/*',
+                            'index.html',
+                            'config.xml'
+                        ]
+                    }
+                ]
+            }
+        },
+
+        clean: ['build'],
+
+        replace: {
+            version: {
+                overwrite: true,
+                src: ['src/config.xml'],
+                replacements: [{
+                    from: /version=\"[0-9]+\.[0-9]+\.[0-9]+(-dev)?\"/g,
+                    to: 'version="<%= pkg.version %>"'
+                }]
+            }
+        },
+
+        jscs: {
+            src: 'src/js/**/*',
+            options: {
+                config: ".jscsrc"
+            }
+        },
+
+        jshint: {
+            options: {
+                jshintrc: true
+            },
+            all: {
+                files: {
+                    src: ['src/js/**/*', 'src/test/**/*', '!src/test/fixtures/']
+                }
+            },
+            grunt: {
+                options: {
+                    jshintrc: '.jshintrc-node'
+                },
+                files: {
+                    src: ['Gruntfile.js']
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks("grunt-jscs");
+    grunt.loadNpmTasks('grunt-text-replace');
+
+    grunt.registerTask('default', [
+        'jshint:grunt',
+        'jshint',
+        'jscs',
+        'replace:version',
+        'compress:widget'
+    ]);
+
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 /*
- *     Copyright (c) 2014 CoNWeT Lab., Universidad Politécnica de Madrid
+ *     Copyright (c) 2014-2015 CoNWeT Lab., Universidad Politécnica de Madrid
  *
- *     This file is part of the googleChart widget.
+ *     This file is part of the googlecharts widget.
  *
  */
 
@@ -14,7 +14,7 @@
     var data = [];
     var graphContainer;
     var graph;
-    var currentType;
+    var currentType = 'LineChart';
     var currentOptions;
 
     var process_input = function process_input(input) {
@@ -42,7 +42,7 @@
         info = JSON.parse(input);
         if (!('type' in info)) {
             // Error. Type is needed
-            console.log('Google Chart Error. "type" is needed');
+            MashupPlatform.widget.log('Google Chart Error. "type" is needed');
             return;
         }
         //if (currentType == null || currentType != info.type) {
@@ -52,14 +52,14 @@
             currentType = info.type;
             if (!('options' in  info)) {
                 // Error. Type is needed
-                console.log('Google Chart Error. "options" is needed');
+                MashupPlatform.widget.log('Google Chart Error. "options" is needed');
                 return;
             }
             // Set graph options
             currentOptions = info.options;
             if (!('data' in  info)) {
                 // Empty graph
-                console.log('Google Chart warning. "data" is empty');
+                MashupPlatform.widget.log('Google Chart warning. "data" is empty');
                 data = [];
             } else {
                 data = info.data;
@@ -74,14 +74,12 @@
             // TODO new Options
             // Normal case
             data = info.data;
-            console.log('Google Chart Update. Data: ' + data);
+            MashupPlatform.widget.log('Google Chart Update. Data: ' + data);
             redraw();
         }
     };
 
     var redraw = function redraw() {
-        // Clean the graph is not necesary
-        //clean_graph();
         graph.draw(google.visualization.arrayToDataTable(data), currentOptions);
     };
 
@@ -99,14 +97,6 @@
         }
     };
 
-    var delete_graph = function delete_graph() {
-        // Clears the chart, and releases all of its allocated resources.
-        currentType = null;
-        currentOptions = null;
-        graph.clearChart();
-        //TODO ¿Habrá que hacer algo más para quitarlo del DOM?
-    };
-
     var clean_graph = function clean_graph() {
         data = [
             ['Time', 'dummy'],
@@ -117,7 +107,7 @@
             title:"No Data",
             width:'100%', height:'100%',
             hAxis: {title: "none"},
-            legend : {position: 'none'}
+            legend: {position: 'none'}
         };
         redraw();
     };
@@ -136,59 +126,14 @@
         }
         redraw();
     };
+    MashupPlatform.widget.context.registerCallback(resizeHandler);
 
     window.addEventListener('load', function () {
         graphContainer = document.getElementById('graphContainer');
         // Resize the linearGraphContainer
         graphContainer.style.height = "100%";
         graphContainer.style.width = "100%";
-        init();
     }, true);
-    window.addEventListener("resize", resizeHandler, true);
-
-    var init = function init() {
-
-        //test
-        var testData = {
-            'type': "ComboChart",
-            'options': {
-                title : 'Monthly Coffee Production by Country',
-                width: '100%',
-                height: '100%',
-                vAxis: {title: "Cups"},
-                hAxis: {title: "Month"},
-                seriesType: "bars",
-                series: {5: {type: "line"}}
-            },
-            'data':[
-                ['Month', 'Bolivia', 'Ecuador', 'Madagascar', 'Papua New Guinea', 'Rwanda', 'Average'],
-                ['2004/05',  165,      938,         522,             998,           450,      614.6],
-                ['2005/06',  135,      1120,        599,             1268,          288,      682],
-                ['2006/07',  157,      1167,        587,             807,           397,      623],
-                ['2007/08',  139,      1110,        615,             968,           215,      609.4],
-                ['2008/09',  136,      691,         629,             1026,          366,      569.6]
-            ]
-        };
-        // Initial Data
-        //process_input(JSON.stringify(testData));
-        // Random Data
-        //setInterval(testFunc, 5000);
-        // Dafault Type
-        currentType = 'LineChart';
-    };
-
-    // Only 4 Tests with ComboChart
-    var testFunc = function(){
-        var newData = [
-            ['Month', 'Bolivia', 'Ecuador', 'Madagascar', 'Papua New Guinea', 'Rwanda', 'Average'],
-            ['2004/05',  Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 900) + 100],
-            ['2005/06',  Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 900) + 100],
-            ['2006/07',  Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 900) + 100],
-            ['2007/08',  Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 900) + 100],
-            ['2008/09',  Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 1000) + 1, Math.floor(Math.random() * 900) + 100]
-        ]
-        process_input(JSON.stringify({'type': "ComboChart",'data': newData}));
-    };
 
     // Input handler
     MashupPlatform.wiring.registerCallback('input', process_input);
